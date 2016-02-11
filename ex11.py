b56dd974e727df69a79928ddee7c130cd0c20b81
gofur <gofur@Admins-iMac.local>
2016-02-11 15:21:39 +0700

    exercise

---
+++ ex11.py
@@ -0,0 +1,10 @@
+x =  "Berapa umur mu?"
+print "%s" % x,
+umur = raw_input()
+print "Berapa tinggi badan mu?",
+tinggi = raw_input()
+y = "Berapa berat badanmu?"
+print "%s" % y,
+berat = raw_input()
+
+print "Jadi umurmu %r umur, %r tinggi and  %r berat" % (umur, tinggi, berat)
---
+++ ex12.py
@@ -0,0 +1,5 @@
+umur = raw_input("Berapakah umurmu?")
+tinggi = raw_input("Berapakah tinggimu?")
+berat = raw_input("Berapa beratmu?")
+
+print "Jadi, umurmu adalah %r, tinggumu %r, dan beratmu %r." % (umur, tinggi, berat)
